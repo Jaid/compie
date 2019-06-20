95ab8cccf6e87f064d79d5189457d91ca541abbc
Jaid <jaid.jsx@gmail.com>
2019-06-20 05:09:00 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,37 +1,50 @@
 import path from "path"
 
 import yargs from "yargs"
+import pascalCase from "pascal-case"
 import fsp from "@absolunet/fsp"
-import {isObjectLike} from "lodash"
+import indexTemplate from "src/templates/index.hbs"
+import styleTemplate from "src/templates/style.hbs"
+import jaidLogger from "jaid-logger"
 
-const getPackageField = async (cwd, field) => {
-  const packageJsonFile = path.join(cwd, "package.json")
-  const packageJsonExists = await fsp.pathExists(packageJsonFile)
-  if (packageJsonExists) {
-    const pkg = await fsp.readJson(packageJsonFile)
-    if (pkg?.[field]) {
-      const value = pkg[field]
-      if (Array.isArray(value)) {
-        return value.join("\n")
-      }
-      if (value |> isObjectLike) {
-        return Object.keys(value).join("\n")
-      }
-      return value
-    }
+const logger = jaidLogger(["Jaid", _PKG_TITLE])
+
+const job = async ({componentsFolder, name, forcePascal}) => {
+  if (forcePascal) {
+    name = name |> pascalCase
   }
-  process.exit(1)
-}
-
-const job = async ({cwd, field}) => {
-  const value = await getPackageField(cwd, field)
-  process.stdout.write(String(value))
+  const componentFolder = path.join(componentsFolder, name)
+  await fsp.ensureDir(componentsFolder)
+  const fileDescriptions = [
+    {
+      name: "index.js",
+      template: indexTemplate,
+    },
+    {
+      name: "style.scss",
+      template: styleTemplate,
+    },
+  ]
+  const writeFileJobs = fileDescriptions.map(async fileDescription => {
+    const content = fileDescription.template({
+      name,
+    })
+    const file = path.join(componentFolder, fileDescription.name)
+    await fsp.outputFile(file, content, "utf8")
+    logger.info("Created file %s", file)
+  })
+  await Promise.all(writeFileJobs)
+  process.exit(0)
 }
 
 const builder = {
-  cwd: {
-    default: process.cwd(),
+  "components-folder": {
+    default: path.join(process.cwd(), "src", "components"),
     type: "string",
   },
+  "force-pascal": {
+    default: false,
+    type: "boolean",
+  },
 }
-yargs.command("$0 <field>", "Returns the value of a package field", builder, job).argv
+yargs.command("$0 <name>", "Creates a React component directory", builder, job).argv
--- test/test.js
+++ test/test.js
@@ -4,14 +4,9 @@ import coffee from "coffee"
 
 const main = path.resolve(process.env.MAIN)
 
-it("should run internal command", () => coffee.fork(main, ["webpackConfigJaid"])
+it("should run internal command", () => coffee.fork(main, ["test-component", "--components-folder", "dist/test/components", "--force-pascal"])
   .expect("code", 0)
-  .expect("stdout", "cli")
-  .debug(true)
-  .end())
-
-it("should run internal command", () => coffee.fork(main, ["scripts"])
-  .expect("code", 0)
-  .expect("stdout", /\ntest\n/s)
+  .expect("stdout", /created file/i)
+  .expect("stdout", /TestComponent[/\\]index\.js/)
   .debug(true)
   .end())
